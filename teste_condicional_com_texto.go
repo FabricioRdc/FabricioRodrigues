7fe532d99cbbc6233aa00d7129a3b55b5c3ad8de
Fabrício Rodrigues <fabriciorodriguesdc@gmail.com>
2026-09-19 14:48:19 -0300

    Update teste_condicional_com_texto.go

--- teste_condicional_com_texto.go
+++ teste_condicional_com_texto.go
@@ -6,13 +6,13 @@ var Batman string
 
 func main () {
 
-     fmt.Println("Diga o seu nome:",nome)
+     fmt.Println("Diga o seu nome:" ,nome)
      fmt.Scan(&nome)
 
      if nome == "Batman" { 
      fmt.Println("Olá,Batman!!!!!")
      } else {
-     fmt.Println("Olá",nome)
+     fmt.Println("Olá" ,nome)
 
 }
 
